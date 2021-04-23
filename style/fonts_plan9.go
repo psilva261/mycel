910bfe3e9d2177b8efde268281233d5d1ed333cd
Philip Silva <philip.silva@protonmail.com>
2021-04-23 12:33:33 +0000

    Use font that maps more characters

--- style/fonts_plan9.go
+++ style/fonts_plan9.go
@@ -9,6 +9,6 @@ import (
 func initFontserver() {}
 
 func (cs Map) FontFilename() string {
-	fontSize := matchClosestFontSize(cs.FontSize(), []int{5,6,7,8,9,10,12,14,16,18,20,24,28,32})
-	return fmt.Sprintf("/lib/font/bit/lucida/unicode.%v.font", fontSize)
+	fontSize := matchClosestFontSize(cs.FontSize(), []int{6,7,8,10,13})
+	return fmt.Sprintf("/lib/font/bit/lucidasans/unicode.%v.font", fontSize)
 }
