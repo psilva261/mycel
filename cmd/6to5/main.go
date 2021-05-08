1d50aea6c14615b107116dac70e71454099ece5d
Philip Silva <philip.silva@protonmail.com>
2021-05-08 17:55:00 +0000

    6to5

---
+++ cmd/6to5/main.go
@@ -0,0 +1,36 @@
+// Conversion of ES6+ into ES5.1 (wip)
+//
+// TODO: turn into a script that uses devjs
+package main
+
+import (
+	"fmt"
+	"github.com/jvatic/goja-babel"
+	"io"
+	"log"
+	"os"
+)
+
+func Main() (err error) {
+	babel.Init(1) // Setup 1 transformer (can be any number > 0)
+	r, err := babel.Transform(os.Stdin, map[string]interface{}{
+		"plugins": []string{
+			"transform-block-scoping",
+			"transform-destructuring",
+			"transform-spread",
+			"transform-parameters",
+		},
+	})
+	if err != nil {
+		return fmt.Errorf("transform: %v", err)
+	}
+	_, err = io.Copy(os.Stdout, r)
+
+	return
+}
+
+func main() {
+	if err := Main(); err != nil {
+		log.Fatalf("%v",err)
+	}
+}
