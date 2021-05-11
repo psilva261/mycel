780d55cb042d7d8e892b25c1368355e0358b1969
Philip Silva <philip.silva@protonmail.com>
2021-05-11 17:43:56 +0000

    tabs -> 4 spaces

--- nodes/nodes.go
+++ nodes/nodes.go
@@ -80,6 +80,7 @@ func NewNodeTree(doc *html.Node, ps style.Map, nodeMap map[*html.Node]style.Map,
 // filterText removes line break runes (TODO: add this later but handle properly) and maps runes to canonical widths
 func filterText(t string) string {
 	t = strings.ReplaceAll(t, "­", "")
+	t = strings.ReplaceAll(t, "\t", "    ")
 	return t
 }
 
